e2051a96d3221994fb12b93c05f3c6140306f7d7
DiligentViking <acore-n@proton.me>
2025-12-15 11:21:04 -0600

    Add custom rules & globalIgnores to eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import globals from "globals";
 import json from "@eslint/json";
 import markdown from "@eslint/markdown";
 import css from "@eslint/css";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
@@ -12,4 +12,10 @@ export default defineConfig([
   { files: ["**/*.json5"], plugins: { json }, language: "json/json5", extends: ["json/recommended"] },
   { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
   { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+  {
+		rules: {
+			"no-unused-vars": "warn",
+		},
+	},
+  globalIgnores(["webpack.*"]),
 ]);
